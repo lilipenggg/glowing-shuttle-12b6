8072b8aac0808a342b0dfaf68947c07792aadc05
Lili Peng <lili.peng@venafi.com>
2018-01-30 18:37:59 -0700

    Finished assignment3

---
+++ ASSIGNMENT3/Controllers/HomeController.cs
@@ -0,0 +1,25 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace CoreApp
+{
+    public class HomeController : Controller
+    {
+        [Route("home/index/{username?}")]
+        public IActionResult Index(string username ="you")
+        {
+            return View(new Greeting { Username = username });
+        }
+
+        [Route("home/greet/{username}")]
+        public IActionResult Greet(string username)
+        {
+            var greeting = new Greeting { Username = username };
+
+            return Ok(greeting);
+        }
+    }
+
+    public class Greeting {
+        public string Username { get; set; }
+    }
+}
