8bea98fa2184e5c80c4aef216204197436bbdefc
Lili Peng <lili.peng@venafi.com>
2018-03-21 19:29:51 -0600

    Added in ViewModels folder to hold some temp object that gets sent back from the form

--- final/web/Controllers/HomeController.cs
+++ final/web/Controllers/HomeController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using web.ViewModels;
 
 // For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -29,7 +30,7 @@ namespace web.Controllers
         }
         
         [HttpPost("contact")]
-        public IActionResult Contact(Object model)
+        public IActionResult Contact(ContactViewModel contactViewModel)
         {
             return View();
         }
