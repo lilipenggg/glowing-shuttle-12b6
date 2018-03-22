d9df1b3069838eb9147fc61636723140fe4019fb
Lili Peng <lili.peng@venafi.com>
2018-03-21 19:09:49 -0600

    Added some basic views and home controller

--- final/web/Startup.cs
+++ final/web/Startup.cs
@@ -15,6 +15,7 @@ namespace web
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddMvc();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -24,6 +25,14 @@ namespace web
             {
                 app.UseDeveloperExceptionPage();
             }
+            else
+            {
+                // Catch error and show the user a friendly error page
+                app.UseExceptionHandler("/error");
+            }
+
+            app.UseMvc();
+            app.UseStaticFiles();
 
             app.Run(async (context) =>
             {
